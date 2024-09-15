aa8583057acdef3ada8cf41d9070d68308aa312c
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2024-09-15 17:28:26 -0300

    "Upando resto dos dias"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,18 @@ export default async function Home() {
           return <Card title={projeto.nome} hora={`${projeto.inicio.slice(0, 5)} - ${projeto.fim.slice(0, 5)}`} local={projeto.local} key={projeto.id} id={projeto.id}/>
         })}
       </Container>
+      <BoxTitle nome="Quinta-Feira" data="19/09/2024"/>
+      <Container>
+        {projetos.filter((projeto) => projeto.data === "2024-09-19")?.map((projeto) => {
+          return <Card title={projeto.nome} hora={`${projeto.inicio.slice(0, 5)} - ${projeto.fim.slice(0, 5)}`} local={projeto.local} key={projeto.id} id={projeto.id}/>
+        })}
+      </Container>
+      <BoxTitle nome="Sexta-Feira" data="20/09/2024"/>
+      <Container>
+        {projetos.filter((projeto) => projeto.data === "2024-09-20")?.map((projeto) => {
+          return <Card title={projeto.nome} hora={`${projeto.inicio.slice(0, 5)} - ${projeto.fim.slice(0, 5)}`} local={projeto.local} key={projeto.id} id={projeto.id}/>
+        })}
+      </Container>
     </div>
   );
 }
