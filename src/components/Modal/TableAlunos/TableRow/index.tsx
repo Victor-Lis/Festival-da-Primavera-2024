4b970326501d28a3a6ca4f6c3ee3ff0a89c357e8
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2024-09-18 09:42:00 -0300

    Trocando cor novamente

--- src/components/Modal/TableAlunos/TableRow/index.tsx
+++ src/components/Modal/TableAlunos/TableRow/index.tsx
@@ -3,9 +3,9 @@ import type{ AlunoType } from "@/@types/AlunoType";
 export default function TableRow({nome, curso, ano, periodo}: AlunoType) {
   return (
     <tr className="bg-blue-500/30">
-      <td className="px-6 py-4 whitespace-nowrap w-2/3 text-white">{nome}</td>
-      <td className="px-6 py-4 whitespace-nowrap max-sm:hidden w-1/6 text-white">{curso}</td>
-      <td className="px-6 py-4 whitespace-nowrap w-1/6 text-white">{ano}</td>
+      <td className="px-6 py-4 whitespace-nowrap w-2/3 text-blue-500">{nome}</td>
+      <td className="px-6 py-4 whitespace-nowrap max-sm:hidden w-1/6 text-blue-500">{curso}</td>
+      <td className="px-6 py-4 whitespace-nowrap w-1/6 text-blue-500">{ano}</td>
     </tr>
   );
 }
